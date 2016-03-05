04fce85b451bcbdebd5d8456aebf7b1327ed0fc1
dennisrcs <denisrcs@gmail.com>
2016-03-05 03:30:12 +0000

    hw 2 part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,27 +18,69 @@ class MoviesController < ApplicationController
       @is_checked = Movie.is_checked_init
     end
 
+    @movies = Movie.all
+    
+    # update ratings in session
     if params.has_key?(:ratings)
-      # update is_checked hash
-      @is_checked = Movie.update_is_checked(params[:ratings])
-      
-      # get keys and filter subset
-      keys = params[:ratings].keys
-      @movies = Movie.where(rating: keys)
-    elsif params.has_key?(:sort_by)
-      # sorting movies according to sort_by param
-      @movies = Movie.order(params[:sort_by]).all
+      session[:ratings] = params[:ratings]
+    end
+    
+    # updating sort_by params in session
+    if params.has_key?(:sort_by)
+      session[:sort_by] = params[:sort_by]
+    end
 
-      # setting hilite class to view's variable
-      if params[:sort_by] == "title"
+    # compare params and session and redirect if necessary
+    check_params_redirect()
+
+    # if there are ratings key, then filter movies
+    if session.has_key?(:ratings)
+      filter_movies(session[:ratings])
+    end
+
+    # if there is a sort_by key, then sort movies
+    if session.has_key?(:sort_by)
+      sort_movies(session[:sort_by])
+    end
+  end
+
+  # if session and params are different, fill params and redirect
+  def check_params_redirect
+    if (session.has_key?(:ratings) ^ params.has_key?(:ratings)) ||
+          (session.has_key?(:sort_by) ^ params.has_key?(:sort_by))
+      # filling data from session
+      parameters = Hash.new
+      parameters[:ratings] = session[:ratings]
+      parameters[:sort_by] = session[:sort_by]
+      
+      # forces flash to keep message
+      flash.keep
+  
+      # redirect with the proper params, to keep RESTfulness
+      redirect_to movies_path(parameters)
+    end
+  end
+
+  # filter movings according to ratings parameter
+  def filter_movies(ratings)
+    # update is_checked hash
+    @is_checked = Movie.update_is_checked(session[:ratings])
+      
+    # get keys and filter subset
+    keys = session[:ratings].keys
+    @movies = Movie.where(rating: keys)
+  end
+
+  # sort movies according to sort_by param
+  def sort_movies(sort_by)
+     # setting proper class
+      if sort_by == "title"
+        @movies = @movies.sort {|a, b| a.title <=> b.title}
         @hilite_title = "hilite"
       else
+        @movies = @movies.sort {|a, b| a.release_date <=> b.release_date}
         @hilite_release_date = "hilite"
       end
-
-    else
-      @movies = Movie.all
-    end
   end
 
   def new
